559d7694f31fb891ef99cab64faf907a95c1c938
xyjsjruiliu <ruiliu1992@gmail.com>
2015-06-01 22:10:48 +0800

    20150601

---
+++ src/main/scala/com/xy/lr/ml/bpnn/BPNeuralNetwork.scala
@@ -0,0 +1,9 @@
+package com.xy.lr.ml.bpnn
+
+/**
+ * Created by xylr on 15-6-1.
+ * BP 神经网络
+ */
+class BPNeuralNetwork {
+  private var number : Int = _
+}
---
+++ src/test/scala/com/xy/lr/ml/bpnn/test/TestMain.scala
@@ -0,0 +1,14 @@
+package com.xy.lr.ml.bpnn.test
+
+import com.xy.lr.ml.bpnn.BPNeuralNetwork
+
+/**
+ * Created by xylr on 15-6-1.
+ * BP 神经网络测试程序
+ */
+object TestMain {
+  def main(args : Array[String]): Unit ={
+    val bpnn = new BPNeuralNetwork()
+
+  }
+}
